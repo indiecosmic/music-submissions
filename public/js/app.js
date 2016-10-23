5dae76f962fc7a121f7ad5c8016f0018eae916e8
Mattias Rudén <mattias@indiesoft.org>
2016-10-23 21:45:27 +0200

    Added require js

--- public/js/app.js
+++ public/js/app.js
@@ -1,64 +1,14 @@
-(function ($) {
-    var app = {
-        onAddLinkButtonClick: function (event) {
-            event.preventDefault();
+requirejs.config({
+    "baseUrl": "/js/lib",
+    "shim": {
+        "bootstrap": { "deps": ['jquery'] }
+    },
+    "paths": {
+        "app": "../",
+        "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min",
+        "bootstrap": "//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min",
+        "font-awesome": "//use.fontawesome.com/3c091cc8ec"
+    }
+});
 
-            var $template = $('#linkTemplate'),
-                $clone = $template
-                    .clone()
-                    .removeClass('hide')
-                    .removeAttr('id');
-            $clone.find('input')
-                .attr('name', 'links[]')
-                .prop('required', true);
-            $clone.insertBefore($template);
-        },
-        onRemoveButtonClick: function (event) {
-            event.preventDefault();
-
-            $(this).parents('.entry:first').remove();
-        },
-        onFormSubmit: function (event) {
-            event.preventDefault();
-            var $submitButton = $('button[type="submit"]');
-            $submitButton
-                .addClass('disabled')
-                .prop('disabled', true);
-
-            $.ajax({
-                url: $(this).attr('action'),
-                method: $(this).attr('method'),
-                data: $(this).serialize(),
-                dataType: 'json'
-            })
-                .done(app.onSubmitSuccess)
-                .fail(app.onSubmitError)
-                .always(app.onSubmitComplete);
-
-        },
-        onSubmitSuccess: function (data, status) {
-            $(".form-group").removeClass('has-error');
-
-            if (data.error) {
-                data.error.forEach(function (error) {
-                    $("input[name='" + error.param + "']").parents('.form-group').addClass('has-error');
-                    $("textarea[name='" + error.param + "']").parents('.form-group').addClass('has-error');
-                });
-                return;
-            }
-            window.location = 'tack';
-        },
-        onSubmitError: function (jqXHR, status, error) {
-            console.error(status, error);
-        },
-        onSubmitComplete: function () {
-            var $submitButton = $('button[type="submit"]');
-            $submitButton
-                .removeClass('disabled')
-                .prop('disabled', false);
-        }
-    };
-    $('#add-links-button').click(app.onAddLinkButtonClick);
-    $(document).on('click', '.btn-remove', app.onRemoveButtonClick);
-    $('form').on('submit', app.onFormSubmit);
-} (jQuery));
+requirejs(["app/main"]);
---
+++ public/js/main.js
@@ -0,0 +1,64 @@
+define(['jquery', 'font-awesome', 'bootstrap'], function ($) {
+    var app = {
+        onAddLinkButtonClick: function (event) {
+            event.preventDefault();
+
+            var $template = $('#linkTemplate'),
+                $clone = $template
+                    .clone()
+                    .removeClass('hide')
+                    .removeAttr('id');
+            $clone.find('input')
+                .attr('name', 'links[]')
+                .prop('required', true);
+            $clone.insertBefore($template);
+        },
+        onRemoveButtonClick: function (event) {
+            event.preventDefault();
+
+            $(this).parents('.entry:first').remove();
+        },
+        onFormSubmit: function (event) {
+            event.preventDefault();
+            var $submitButton = $('button[type="submit"]');
+            $submitButton
+                .addClass('disabled')
+                .prop('disabled', true);
+
+            $.ajax({
+                url: $(this).attr('action'),
+                method: $(this).attr('method'),
+                data: $(this).serialize(),
+                dataType: 'json'
+            })
+                .done(app.onSubmitSuccess)
+                .fail(app.onSubmitError)
+                .always(app.onSubmitComplete);
+
+        },
+        onSubmitSuccess: function (data, status) {
+            $(".form-group").removeClass('has-error');
+
+            if (data.error) {
+                data.error.forEach(function (error) {
+                    $("input[name='" + error.param + "']").parents('.form-group').addClass('has-error');
+                    $("textarea[name='" + error.param + "']").parents('.form-group').addClass('has-error');
+                });
+                return;
+            }
+            window.location = 'tack';
+        },
+        onSubmitError: function (jqXHR, status, error) {
+            console.error(status, error);
+        },
+        onSubmitComplete: function () {
+            var $submitButton = $('button[type="submit"]');
+            $submitButton
+                .removeClass('disabled')
+                .prop('disabled', false);
+        }
+    };
+    $('#add-links-button').click(app.onAddLinkButtonClick);
+    $(document).on('click', '.btn-remove', app.onRemoveButtonClick);
+    $('form').on('submit', app.onFormSubmit);
+});
